bb1e53921354bdd89d04b89f0e2fe3809b5d0002
huyifan <854152389@qq.com>
2025-10-13 14:52:16 +0800

    ✨ feat: 添加用户认证功能，集成会话管理和数据库连接，更新路由以支持登录和注册

--- express/express.js
+++ express/express.js
@@ -1,9 +1,27 @@
 const express = require('express') // 引入express模块
 const fs = require('fs') // 引入fs模块
-const bodyParser = require('body-parser') // 引入body-parser模块
 const homeRouter = require('./routes/homeRouter') // 引入home路由
+const cookieParser = require('cookie-parser') // 引入cookie-parser模块
+const session = require('express-session') // 引入express-session模块
+const MongoStore = require('connect-mongo') // 引入connect-mongo模块
+const db = require('./db/db')
+const config = require('./config/config')
 
 const app = express() // 创建express实例
+
+app.use(cookieParser()) // 使用cookie-parser中间件
+
+app.use(session({
+    name: 'sid', // 设置cookie的名称
+    secret: 'secret', // 设置session的密钥
+    saveUninitialized: false, // 设置session的初始化
+    resave: true, // 设置session的重新保存
+    store: MongoStore.create({ mongoUrl: `mongodb://${config.DBHOST}:${config.DBPORT}/${config.DBNAME}` }), // 设置session的存储方式，使用connect-mongo模块
+    cookie: {
+        httpOnly: true, // 设置cookie的httpOnly属性
+        maxAge: 1000 * 60 * 10 // 设置cookie的过期时间
+    } // 设置cookie的配置
+})) // 使用express-session中间件
 
 // 中间件，记录请求的url和ip
 function recordMiddleware(req, res, next) {
@@ -28,13 +46,12 @@ app.use(recordMiddleware) // 使用中间件
 
 app.use(homeRouter) // 使用homeRouter路由
 
-app.use(express.static(__dirname + '/')) // 设置静态文件目录
+app.use(express.static(__dirname + '/views')) // 设置静态文件目录
 
-const jsonParser = bodyParser.json() // 解析json格式的请求体
-const urlencodedParser = bodyParser.urlencoded({ extended: false }) // 解析urlencoded格式的请求体
-
-app.post('/login', urlencodedParser, (req, res) => {
-    res.end('登录成功' + req.body.username + ' ' + req.body.password)
+db((mongoose) => {
+    console.log('mongoose连接成功')
+}, (err) => {
+    console.log(err)
 })
 
 app.listen(9000, () => {
--- express/routes/homeRouter.js
+++ express/routes/homeRouter.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
+const bodyParser = require('body-parser') // 引入body-parser模块
+const UserModel = require('../models/UserModel')
 const router = express.Router();
+const checkLoginMiddleware = require('../middlewares/checkLoginMiddleware')
 
-router.get('/home', (req, res) => {
-    res.send('home')
+router.get('/home', checkLoginMiddleware, (req, res) => {
+    res.send(`<h1>欢迎你，${req.session.username}，id为${req.session._id}</h1>`)
 })
 
 router.get('/', (req, res) => {
+    res.redirect('/home')
     // console.log(req.method)
     // console.log(req.url)
     // console.log(req.httpVersion)
@@ -29,7 +32,7 @@ router.get('/', (req, res) => {
     //     'Content-Type': 'text/html;charset=utf-8'
     // })
     // res.send('默认首页')
-    res.status(200).set({ 'Content-Type': 'text/html;charset=utf-8' }).send('默认首页')
+    // res.status(200).set({ 'Content-Type': 'text/html;charset=utf-8' }).send('默认首页')
 })
 
 // router.get('/:id.html', (req, res) => {
@@ -44,12 +47,91 @@ router.get('/', (req, res) => {
 //     res.sendFile(__dirname + '/index.html')
 // })
 
-router.all('/test', (req, res) => {
+router.all('/test', checkLoginMiddleware, (req, res) => {
     res.end('test')
 })
 
 // router.all('/*', (req, res) => {
 //     res.end('404')
 // })
+
+router.get('/setCookie', (req, res) => {
+    res.cookie('name', 'zhangsan', {
+        maxAge: 1000 * 60 * 10
+    })
+    res.send('setCookie')
+})
+
+router.get('/getCookie', (req, res) => {
+    console.log(req.cookies)
+    res.send('getCookie')
+})
+
+router.get('/clearCookie', (req, res) => {
+    res.clearCookie('name')
+    res.send('clearCookie')
+})
+
+// router.get('/login', (req, res) => {
+//     if (req.query?.username === 'admin' && req.query?.password === '123456') {
+//         req.session.username = req.query.username
+//         res.send('get测试登录成功')
+//     } else {
+//         res.send('get测试登录失败,请输入正确的用户名和密码')
+//     }
+// })
+
+const jsonParser = bodyParser.json() // 解析json格式的请求体
+const urlencodedParser = bodyParser.urlencoded({ extended: false }) // 解析urlencoded格式的请求体
+
+router.post('/login', urlencodedParser, async (req, res) => {
+    if (req.body?.username && req.body?.password) {
+        const data = await UserModel.findOne({ username: req.body.username, password: req.body.password })
+        if (data) {
+            req.session.username = data.username
+            req.session._id = data._id
+            res.redirect('/home')
+        } else {
+            res.send('登录失败')
+        }
+    } else {
+        res.send('请输入用户名和密码')
+    }
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.send('退出登录成功')
+    })
+})
+
+router.get('/reg', (req, res) => {
+    if (req.query?.username && req.query?.password) {
+        res.send('get测试注册成功')
+    } else {
+        res.send('get注册失败')
+    }
+})
+
+router.post('/reg', urlencodedParser, async (req, res) => {
+    if (req.body?.username && req.body?.password) {
+        if (await UserModel.findOne({ username: req.body.username })) {
+            res.send('用户名已存在')
+        } else {
+            UserModel.create({
+                username: req.body.username,
+                password: req.body.password
+            }).then(res1 => {
+                console.log(res1)
+                res.send('注册成功')
+            }).catch(err => {
+                console.log(err)
+                res.send('注册失败')
+            })
+        }
+    } else {
+        res.send('请输入用户名和密码')
+    }
+})
 
 module.exports = router
